1fb8fbbf64f538b8d890b2bafa1b6f8497fdcdd5
Liisa-Lotte Pehter <pehterli@ut.ee>
2024-04-12 11:36:12 +0300

    React client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    async function fetchApplications() {
+      try{
+        const getAllApplications = await axios.get('http://localhost:8080/applications')
+        setApplications(getAllApplications.data);
+      }catch (error){
+        console.log(error)
+      }
+    }fetchApplications();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Applications</h1>
+        <table>
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Candidate name</th>
+            <th>Updated on</th>
+            <th>Interview type</th>
+            <th>Interviewer name</th>
+          </tr>
+          </thead>
+          <tbody>
+            {applications.map(application => (
+              <tr key={application.id}>
+                <td>{application.id}</td>
+                <td>{application.applicationState}</td>
+                <td>{application.candidate.firstName + " " + application.candidate.lastName}</td>
+                <td>{application.updatedOn}</td>
+                {application.interview != null ? (
+                    <td>{application.interview.interviewType}</td>
+                  ): (
+                    <td>null</td>
+                )}
+                {application.interview != null ? (
+                    <td>{application.interview.interviewerName}</td>
+                ): (
+                    <td>null</td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
   );
 }
 
